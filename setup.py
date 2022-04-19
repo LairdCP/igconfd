3296c99067ed507b962ea4ef8f22a86dd665e25f
Boris Krasnovskiy <boris.krasnovskiy@lairdconnect.com>
2022-04-19 11:29:23 -0400

    igconfd: Reformatted with black
    Bug 21274

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup
 
 setup(
-    name='igconfd',
-    version='1.0',
-    packages=['igconfd'],
-    scripts=['scripts/igconfd'],
+    name="igconfd",
+    version="1.0",
+    packages=["igconfd"],
+    scripts=["scripts/igconfd"],
 )
